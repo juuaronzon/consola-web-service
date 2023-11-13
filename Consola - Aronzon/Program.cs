7a1d4ca6197531156cb7b4da5c9bfc9dc1b0ac10
juuaronzon <131707113+juuaronzon@users.noreply.github.com>
2023-11-13 11:52:55 -0300

    Add files via upload

---
+++ Consola - Aronzon/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsumoWebService2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SOAP.CountryInfoService client = new SOAP.CountryInfoService();
+
+            var respuesta = client.FullCountryInfoAllCountries();
+            Console.WriteLine(respuesta.FirstOrDefault().sContinentCode);
+            Console.WriteLine(respuesta.FirstOrDefault().sName);
+            Console.WriteLine(respuesta.FirstOrDefault().sCapitalCity);
+            Console.WriteLine(respuesta.FirstOrDefault().sCountryFlag);
+            Console.WriteLine(respuesta.FirstOrDefault().sISOCode);
+
+            Console.Read();
+
+        }
+    }
+}
